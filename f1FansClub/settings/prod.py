a05e8369288ddefecd15a4c15a2fb1eb0d121fba
Yen <s810643@gmail.com>
2022-07-06 00:56:46 +0800

    Fix TYPO

--- f1FansClub/settings/prod.py
+++ f1FansClub/settings/prod.py
@@ -13,5 +13,5 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # heroku
-django_on_heroku.settings(locals(), staticfiles == False)
+django_on_heroku.settings(locals(), staticfiles=False)
 del DATABASES['default']['OPETIONS']['sslmode']
